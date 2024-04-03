f9e9e2bd4c6751d4b1c7a0e1e738c1235b1439b8
Reywan18 <anhquana5k44nknd@gmail.com>
2024-04-03 10:41:16 +0700

    bt tuan 2

---
+++ app/src/main/java/com/example/myapplication/tuan2/Tuan22MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.tuan2;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+
+public class Tuan22MainActivity extends AppCompatActivity {
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tuan22_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        lv=findViewById(R.id.Tuan22ListView);
+        String[] arr=new String[]{"Mon 1", "Mon 2", "Mon 3", "Mon 4", "Mon 5"};
+        ArrayAdapter<String>
+                adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
+        lv.setAdapter(adapter);
+    }
+}
